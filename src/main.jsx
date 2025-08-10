a43120f4a5a9c1c5969904ceb85f7cf3e9823f0f
K1butsujiMuzan <maks_shishkin_2007@inbox.ru>
2025-08-10 20:16:01 +0200

    css settings

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
+import "./styles/index.css"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router"
-import "./styles/index.css"
 
 
 createRoot(document.getElementById('root')).render(
