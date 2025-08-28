2a93698490028fdca5837cbdaf8ba95a6c4e5e02
K1butsujiMuzan <maks_shishkin_2007@inbox.ru>
2025-08-28 13:06:32 +0200

    about us optimized

--- src/components/AboutUsContainer/AboutUsContainer.jsx
+++ src/components/AboutUsContainer/AboutUsContainer.jsx
@@ -1,48 +1,8 @@
 import styles from "./AboutUsContainer.module.css"
 import FoodAboutCard from "@components/AboutUsContainer/parts/FoodAboutCard/FoodAboutCard";
-
-import sushi from "@/assets/images/AboutUs/Cards/Sushi.svg"
-import pizza from "@/assets/images/AboutUs/Cards/Pizza.svg"
-import noodles from "@/assets/images/AboutUs/Cards/Noodles.svg"
-import salad from "@/assets/images/AboutUs/Cards/Salad.svg"
-import iceCream from "@/assets/images/AboutUs/Cards/IceCream.svg"
-import cola from "@/assets/images/AboutUs/Cards/Cola.svg"
-
-import rollsImage from "@/assets/images/AboutUs/Galery/1.png"
-import drinkImage from "@/assets/images/AboutUs/Galery/2.png"
-import pizzaImage from "@/assets/images/AboutUs/Galery/3.png"
-import noodlesImage from "@/assets/images/AboutUs/Galery/4.png"
-import dessertImage from "@/assets/images/AboutUs/Galery/5.png"
-import sushiImage from "@/assets/images/AboutUs/Galery/6.png"
+import {cards, images} from "@components/AboutUsContainer/AboutUsImagex";
 
 function AboutUsContainer() {
-  const cards = [
-    {
-      text: "Роллы и суши из лучших ингредиентов",
-      image: sushi,
-    },
-    {
-      text: "Ароматные пиццы с хрустящей корочкой",
-      image: pizza,
-    },
-    {
-      text: "Сытные воки с яркими азиатскими соусами",
-      image: noodles,
-    },
-    {
-      text: "Лёгкие салаты и закуски для компании",
-      image: salad,
-    },
-    {
-      text: "Нежные и изысканные десерты",
-      image: iceCream,
-    },
-    {
-      text: "Освежающие и согревающие напитки",
-      image: cola,
-    }
-  ]
-
   return(
     <div className={`${styles.AboutUsInner} container`}>
       <div className={styles.textCard}>
@@ -62,19 +22,33 @@ function AboutUsContainer() {
             />
           ))}
         </ul>
-        <div className={styles.AboutGalleryImages}>
-          <div className={styles.galleryRow}>
-            <img src={rollsImage}/>
-            <img src={drinkImage}/>
-          </div>
-          <div className={styles.galleryRow}>
-            <img src={pizzaImage}/>
-            <img src={noodlesImage}/>
-          </div>
-          <div className={styles.galleryRow}>
-            <img src={dessertImage}/>
-            <img src={sushiImage}/>
-          </div>
+        <div
+          className={styles.AboutGalleryImages}
+          aria-hidden={true}
+        >
+          {images.map((row, indexRow) => (
+            <div
+              className={styles.galleryRow}
+              key={indexRow}
+            >
+              {row.map((image, indexImage) => (
+                <picture key={indexImage}>
+                  {image.sources.map((source, indexSource) => (
+                    <source
+                      key={source.srcSet}
+                      srcSet={source.srcSet}
+                      type={source.type}
+                    />
+                  ))}
+                  <img
+                    src={image.png}
+                    key={image.png}
+                    loading={"lazy"}
+                  />
+                </picture>
+              ))}
+            </div>
+          ))}
         </div>
       </div>
     </div>
---
+++ src/components/AboutUsContainer/AboutUsImagex.js
@@ -0,0 +1,103 @@
+import sushi from "@/assets/images/AboutUs/Cards/Sushi.svg"
+import pizza from "@/assets/images/AboutUs/Cards/Pizza.svg"
+import noodles from "@/assets/images/AboutUs/Cards/Noodles.svg"
+import salad from "@/assets/images/AboutUs/Cards/Salad.svg"
+import iceCream from "@/assets/images/AboutUs/Cards/IceCream.svg"
+import cola from "@/assets/images/AboutUs/Cards/Cola.svg"
+
+import rollsImage_png from "@/assets/images/AboutUs/Galery/1.png"
+import rollsImage_webp from "@/assets/images/AboutUs/Galery/1.webp"
+import rollsImage_avif from "@/assets/images/AboutUs/Galery/1.avif"
+import drinkImage_png from "@/assets/images/AboutUs/Galery/2.png"
+import drinkImage_webp from "@/assets/images/AboutUs/Galery/2.webp"
+import drinkImage_avif from "@/assets/images/AboutUs/Galery/2.avif"
+import pizzaImage_png from "@/assets/images/AboutUs/Galery/3.png"
+import pizzaImage_webp from "@/assets/images/AboutUs/Galery/3.webp"
+import pizzaImage_avif from "@/assets/images/AboutUs/Galery/3.avif"
+import noodlesImage_png from "@/assets/images/AboutUs/Galery/4.png"
+import noodlesImage_webp from "@/assets/images/AboutUs/Galery/4.webp"
+import noodlesImage_avif from "@/assets/images/AboutUs/Galery/4.avif"
+import dessertImage_png from "@/assets/images/AboutUs/Galery/5.png"
+import dessertImage_webp from "@/assets/images/AboutUs/Galery/5.webp"
+import dessertImage_avif from "@/assets/images/AboutUs/Galery/5.avif"
+import sushiImage_png from "@/assets/images/AboutUs/Galery/6.png"
+import sushiImage_webp from "@/assets/images/AboutUs/Galery/6.webp"
+import sushiImage_avif from "@/assets/images/AboutUs/Galery/6.avif"
+
+export const cards = [
+  {
+    text: "Роллы и суши из лучших ингредиентов",
+    image: sushi,
+  },
+  {
+    text: "Ароматные пиццы с хрустящей корочкой",
+    image: pizza,
+  },
+  {
+    text: "Сытные воки с яркими азиатскими соусами",
+    image: noodles,
+  },
+  {
+    text: "Лёгкие салаты и закуски для компании",
+    image: salad,
+  },
+  {
+    text: "Нежные и изысканные десерты",
+    image: iceCream,
+  },
+  {
+    text: "Освежающие и согревающие напитки",
+    image: cola,
+  }
+]
+
+export const images = [
+  [
+    {
+      sources: [
+        {srcSet: rollsImage_avif, type: "image/avif"},
+        {srcSet: rollsImage_webp, type: "image/webp"}
+      ],
+      png: rollsImage_png
+    },
+    {
+      sources: [
+        {srcSet: drinkImage_avif, type: "image/avif"},
+        {srcSet: drinkImage_webp, type: "image/webp"}
+      ],
+      png: drinkImage_png
+    }
+  ],
+  [
+    {
+      sources: [
+        {srcSet: pizzaImage_avif, type: "image/avif"},
+        {srcSet: pizzaImage_webp, type: "image/webp"}
+      ],
+      png: pizzaImage_png
+    },
+    {
+      sources: [
+        {srcSet: noodlesImage_avif, type: "image/avif"},
+        {srcSet: noodlesImage_webp, type: "image/webp"}
+      ],
+      png: noodlesImage_png
+    }
+  ],
+  [
+    {
+      sources: [
+        {srcSet: dessertImage_avif, type: "image/avif"},
+        {srcSet: dessertImage_webp, type: "image/webp"}
+      ],
+      png: dessertImage_png
+    },
+    {
+      sources: [
+        {srcSet: sushiImage_avif, type: "image/avif"},
+        {srcSet: sushiImage_webp, type: "image/webp"}
+      ],
+      png: sushiImage_png
+    }
+  ]
+]
