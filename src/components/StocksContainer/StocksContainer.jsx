e0916abe201d996e01e4caea7f5b8022cce79dae
K1butsujiMuzan <maks_shishkin_2007@inbox.ru>
2025-09-05 21:17:51 +0200

    feat(StocksContainer): created elements

--- src/components/StocksContainer/StocksContainer.jsx
+++ src/components/StocksContainer/StocksContainer.jsx
@@ -1,10 +1,44 @@
 import styles from "./StocksContainer.module.css"
 import PageLink from "@components/UI/PageLink/PageLink";
+import {cards} from "@components/StocksContainer/stocks";
+import TextBlock from "@components/UI/TextBlock/TextBlock";
 
 function StocksContainer() {
   return(
     <section className={"container pageContainer"}>
       <PageLink to={"/stocks"} text={"Акции"}/>
+      <TextBlock>
+        <h1 className={"pageTitle"}>Акции — «Джонни Тунец»</h1>
+      </TextBlock>
+      <div className={styles.cardsContainer}>
+        {cards.map((card, index) => (
+          <div
+            key={index}
+            className={styles.stockCard}
+          >
+            <div className={styles.stockCardUp}>
+              <picture>
+                {card.source.map((source, _) => (
+                  <source
+                    key={source.srcSet}
+                    srcSet={source.srcSet}
+                    type={source.type}
+                  />
+                ))}
+                <img
+                  className={styles.cardImage}
+                  src={card.png}
+                  alt={card.alt}
+                  width={303}
+                  height={160}
+                />
+              </picture>
+              <h2 className={styles.cardText}>{card.text}</h2>
+            </div>
+            <span className={styles.cardShow}>Подробнее</span>
+          </div>
+        ))}
+      </div>
     </section>
   )
 }
---
+++ src/components/StocksContainer/stocks.js
@@ -0,0 +1,113 @@
+import img1_png from "@/assets/images/Stocks/1.png"
+import img2_png from "@/assets/images/Stocks/2.png"
+import img3_png from "@/assets/images/Stocks/3.png"
+import img4_png from "@/assets/images/Stocks/4.png"
+import img5_png from "@/assets/images/Stocks/5.png"
+import img6_png from "@/assets/images/Stocks/6.png"
+import img7_png from "@/assets/images/Stocks/7.png"
+import img8_png from "@/assets/images/Stocks/8.png"
+import img9_png from "@/assets/images/Stocks/9.png"
+
+import img1_webp from "@/assets/images/Stocks/1.webp"
+import img2_webp from "@/assets/images/Stocks/2.webp"
+import img3_webp from "@/assets/images/Stocks/3.webp"
+import img4_webp from "@/assets/images/Stocks/4.webp"
+import img5_webp from "@/assets/images/Stocks/5.webp"
+import img6_webp from "@/assets/images/Stocks/6.webp"
+import img7_webp from "@/assets/images/Stocks/7.webp"
+import img8_webp from "@/assets/images/Stocks/8.webp"
+import img9_webp from "@/assets/images/Stocks/9.webp"
+
+import img1_avif from "@/assets/images/Stocks/1.avif"
+import img2_avif from "@/assets/images/Stocks/2.avif"
+import img3_avif from "@/assets/images/Stocks/3.avif"
+import img4_avif from "@/assets/images/Stocks/4.avif"
+import img5_avif from "@/assets/images/Stocks/5.avif"
+import img6_avif from "@/assets/images/Stocks/6.avif"
+import img7_avif from "@/assets/images/Stocks/7.avif"
+import img8_avif from "@/assets/images/Stocks/8.avif"
+import img9_avif from "@/assets/images/Stocks/9.avif"
+
+export const cards = [
+  {
+    source: [
+      {srcSet: img1_avif, type: "image/avif"},
+      {srcSet: img1_webp, type: "image/webp"}
+    ],
+    png: img1_png,
+    alt: "Гарантия качества",
+    text: "Акция - «Гарантия качества»"
+  },
+  {
+    source: [
+      {srcSet: img2_avif, type: "image/avif"},
+      {srcSet: img2_webp, type: "image/webp"}
+    ],
+    png: img2_png,
+    alt: "Счастливые часы",
+    text: "Акция - «Счастливые часы 10%»"
+  },
+  {
+    source: [
+      {srcSet: img3_avif, type: "image/avif"},
+      {srcSet: img3_webp, type: "image/webp"}
+    ],
+    png: img3_png,
+    alt: "Подарок за отзыв",
+    text: "Акция - «Подарки за отзыв»"
+  },
+  {
+    source: [
+      {srcSet: img4_avif, type: "image/avif"},
+      {srcSet: img4_webp, type: "image/webp"}
+    ],
+    png: img4_png,
+    alt: "Топпинг Оплачивается отдельно",
+    text: "Внимание - Топпинги"
+  },
+  {
+    source: [
+      {srcSet: img5_avif, type: "image/avif"},
+      {srcSet: img5_webp, type: "image/webp"}
+    ],
+    png: img5_png,
+    alt: "Подарок к заказу",
+    text: "Акция - «Подарок к первому заказу»"
+  },
+  {
+    source: [
+      {srcSet: img6_avif, type: "image/avif"},
+      {srcSet: img6_webp, type: "image/webp"}
+    ],
+    png: img6_png,
+    alt: "Пицца за 1 рубль",
+    text: "Акция - «Пицца за 1₽»"
+  },
+  {
+    source: [
+      {srcSet: img7_avif, type: "image/avif"},
+      {srcSet: img7_webp, type: "image/webp"}
+    ],
+    png: img7_png,
+    alt: "Получай кешбэк",
+    text: "Акция - «Кэшбэк»"
+  },
+  {
+    source: [
+      {srcSet: img8_avif, type: "image/avif"},
+      {srcSet: img8_webp, type: "image/webp"}
+    ],
+    png: img8_png,
+    alt: "Самовывоз скидка 10%",
+    text: "Акция - «Самовывоз»"
+  },
+  {
+    source: [
+      {srcSet: img9_avif, type: "image/avif"},
+      {srcSet: img9_webp, type: "image/webp"}
+    ],
+    png: img9_png,
+    alt: "Подарок на день рождения",
+    text: "Акция - «День рождения»"
+  },
+]
